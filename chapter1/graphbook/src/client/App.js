5b2140c32a59dc940a98ed4aa585c8f8bab55234
Pick1a1username <xscreensaver@outlook.com>
2022-07-13 21:08:07 +0900

    the form for new post added.

--- chapter1/graphbook/src/client/App.js
+++ chapter1/graphbook/src/client/App.js
@@ -8,8 +8,32 @@ import avatar2 from '../../uploads/avatar2.png';
 
 const App = () => {
     const [posts, setPosts] = useState(initialPosts);
+    const [postContent, setPostContent] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newPost = {
+            id: posts.length + 1,
+            text: postContent,
+            user: {
+                avatar: avatar1,
+                username: 'Fake User'
+            }
+        };
+        setPosts([newPost, ...posts]);
+        setPostContent('');
+    };
+
     return (
         <div className="container">
+            <div className="postForm">
+                <form onSubmit={handleSubmit}>
+                    <textarea value={postContent} onChange={(e) => 
+                        setPostContent(e.target.value)}
+                        placeholder="Write your custom post!"/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
             <div className="feed">
                 { posts.map((post, i) => 
                     <div key={post.id} className="post">
@@ -28,22 +52,22 @@ const App = () => {
 }
 
 const initialPosts = [
-    {
-        id: 2,
-        text: 'Lorem ipsum',
-        user: {
-        avatar: avatar1,
-        username: 'Test User'
-        }
-    },
-    {
-        id: 1,
-        text: 'Lorem ipsum',
-        user: {
-        avatar: avatar2,
-        username: 'Test User 2'
-        }
-    }
+    {
+        id: 2,
+        text: 'Lorem ipsum',
+        user: {
+        avatar: avatar1,
+        username: 'Test User'
+        }
+    },
+    {
+        id: 1,
+        text: 'Lorem ipsum',
+        user: {
+        avatar: avatar2,
+        username: 'Test User 2'
+        }
+    }
 ];
 
 export default App
