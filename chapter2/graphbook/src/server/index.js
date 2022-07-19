ab3499eb49201b3c3321c3521584800a501d8245
Pick1a1username <xscreensaver@outlook.com>
2022-07-19 22:36:33 +0900

    compress, cors, and helmet added to server.

--- chapter2/graphbook/src/server/index.js
+++ chapter2/graphbook/src/server/index.js
@@ -1,13 +1,30 @@
+import compress from 'compression';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 import path from 'path';
 
 const app = express();
+
+app.use(compress());
+app.use(cors());
 
 const root = path.join(__dirname, '../../');
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(root, '/dist/client/index.html'));
+    res.sendFile(path.join(root, '/dist/client/index.html'));
 });
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        // imgSrc: ["'self'", "data:", "*.amazonaws.com"] // I will not use AWS.
+    }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
