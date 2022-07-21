79e12c7d603d65066889395006874edbade7bb9f
Pick1a1username <xscreensaver@outlook.com>
2022-07-21 20:54:54 +0900

    user type added to graphql server.

--- chapter2/graphbook/src/server/services/graphql/schema.js
+++ chapter2/graphbook/src/server/services/graphql/schema.js
@@ -1,7 +1,12 @@
 const typeDefinitions = `
     type Post {
         id: Int
+        user: User
         text: String
+    }
+    type User {
+        avatar: String
+        username: String
     }
     type RootQuery {
         posts: [Post]
