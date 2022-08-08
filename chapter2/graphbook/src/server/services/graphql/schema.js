24dc20dd9fa467415706bd6931d10db379fe1938
Pick1a1username <xscreensaver@outlook.com>
2022-08-08 21:54:44 +0900

    logger added. posts can be added by graphql.

--- chapter2/graphbook/src/server/services/graphql/schema.js
+++ chapter2/graphbook/src/server/services/graphql/schema.js
@@ -11,8 +11,22 @@ const typeDefinitions = `
     type RootQuery {
         posts: [Post]
     }
+    input PostInput {
+        text: String!
+    }
+    input UserInput {
+        username: String!
+        avatar: String!
+    }
+    type RootMutation {
+        addPost (
+            post: PostInput!
+            user: UserInput!
+        ): Post
+    }
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 `;
 export default [typeDefinitions];
